d834cc889308af4c87f5ff2ccee2b4e0b572d4f3
Iustin Sandu <idsandu@gmail.com>
2018-04-23 19:47:58 +0100

    Fixed var name typo in 1:no_of_positions

--- ReadXL2RTData.m
+++ ReadXL2RTData.m
@@ -2,9 +2,9 @@ function RTMatrix = ReadXL2RTData(fno_start, f1, f2, no_of_positions, verbose)
 % A script that reads RT60 measured data from an NTI XL2 Audio measurement 
 % device and returns a 3D matrix with all RT60 data. Each row represents a 
 % frequency bin, with the collumn structure as [Freq(Hz), RTtime]. The 3rd
-% dimension creates a 2D array in the form described for each position. 
+% dimension stores a 2D array in the form described for each position. 
 % 
-% For invalid values (-.--), the RT time returned it 999. 
+% For invalid values (-.--), the RT time returned is 999. 
 %
 % The paramaters must give it the broken down filename and the starting
 % numnber for the report filenames. This is easier shown than explained:
@@ -22,7 +22,7 @@ function RTMatrix = ReadXL2RTData(fno_start, f1, f2, no_of_positions, verbose)
 %    myMatrix = ReadXL2RTData(fno_start, f1, f2, no_of_positions, verbose);
 % ----------------------------------------
 % 
-% TODO: catch if 2 consecutive files have been deleted. At the moment works
+% TODO: catch if 2 consecutive files have been deleted. At the moment it  works
 % for 1 deleted file in between 2 existing ones. 
 % TODO2: find a nicer data structure so the frequency bins are only stored
 % once and not once per position.
@@ -45,7 +45,7 @@ function RTMatrix = ReadXL2RTData(fno_start, f1, f2, no_of_positions, verbose)
                              % text file names may not be consecutive 
                              % numbers
 
-    for pos=1:no_of_position    % Changes every time a new file is loaded (a.i. new position)
+    for pos=1:no_of_positions    % Changes every time a new file is loaded (a.i. new position)
         if filenumber<10
             fmid = strcat('00',num2str(filenumber));
         elseif filenumber<100
@@ -66,8 +66,8 @@ function RTMatrix = ReadXL2RTData(fno_start, f1, f2, no_of_positions, verbose)
             end
 
         else
-            % file doesn't exist so 
-            % maybe you've deleted that particular file but the next one is still good
+            % file doesn't exist, so maybe you've deleted that 
+	    % particular file, but the next one is still good
             fprintf('WARNING: File %s is missing, data integrity may be compromised!\n', filename);
 
             % try opening the next file along
